5a5f0ff8b528ba3026d88c09b320d5cdd8c44948
Isa-josep <isaurini1902@gmail.com>
2024-01-09 21:37:10 -0600

    NowPlaying Provider y Notifier

---
+++ lib/presentation/provider/movies/movies_providers.dart
@@ -0,0 +1,31 @@
+import 'package:cinepolis/domain/entities/movie.dart';
+import 'package:cinepolis/presentation/provider/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//peliculas que estane en cartelera
+final nowPlayingMovisProvider= StateNotifierProvider<MoviesNotifier,List<Movie>>((ref) {
+  
+  final fetchMoreMovies=ref.watch(movieRepositoryProvider).getNowPlaying;
+
+  return MoviesNotifier(
+    fetchMoreMovies: fetchMoreMovies
+  );
+});
+
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage=0;
+  MovieCallback fetchMoreMovies;
+
+
+  MoviesNotifier({
+    required this.fetchMoreMovies,
+  }):super([]);
+
+  Future <void>  loadNexPage() async{
+    currentPage++;
+    final List<Movie> movies=await fetchMoreMovies(page: currentPage);
+    state=[...state,...movies];
+  } 
+}
