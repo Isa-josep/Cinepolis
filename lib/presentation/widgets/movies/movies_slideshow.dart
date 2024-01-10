dcc63dca2f0b563719ff8eac3919b1543a76e24c
Isa-josep <isaurini1902@gmail.com>
2024-01-10 00:22:03 -0600

    card moved

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinepolis/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,7 @@ class MoviesSlideShow extends StatelessWidget {
       width: double.infinity,
       child: 
         Swiper(
+
           viewportFraction: 0.8,//or 0.9
           scale: 0.9,
           autoplay: true,
@@ -35,6 +37,36 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+       boxShadow: const [
+         BoxShadow(
+          color: Colors.black26, //45??
+          blurRadius: 10,
+          offset: Offset(0, 10),
+        )
+      ]
+    );
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {  //no jalo esta madre
+              if(loadingProgress!=null){
+                return const DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.black12)
+                );
+              }
+              return FadeIn(child: child);
+            }
+          )
+        )
+      ),
+    );
   }
 }
