165dbec170e93e66859ac9b9d5b28590e4b8cfe5
Isa-josep <isaurini1902@gmail.com>
2024-01-09 22:40:44 -0600

    movie Response fix

--- lib/infrastructure/models/moviedb/moviedb_response.dart
+++ lib/infrastructure/models/moviedb/moviedb_response.dart
@@ -1,47 +1,46 @@
 import 'movie_moviedb.dart';
 
-class MovieDbResponse {
-    final Dates? dates;
-    final int page;
-    final List<MovieMovieDB> MovieMovieDBs;
-    final int totalPages;
-    final int totalMovieMovieDBs;
 
+class MovieDbResponse {
     MovieDbResponse({
         required this.dates,
         required this.page,
-        required this.MovieMovieDBs,
+        required this.results,
         required this.totalPages,
-        required this.totalMovieMovieDBs,
+        required this.totalResults,
     });
 
+    final Dates? dates;
+    final int page;
+    final List<MovieMovieDB> results;
+    final int totalPages;
+    final int totalResults;
+
     factory MovieDbResponse.fromJson(Map<String, dynamic> json) => MovieDbResponse(
-        dates: json["dates"] ? Dates.fromJson(json["dates"]):null,
+        dates: json["dates"] != null ? Dates.fromJson(json["dates"]) : null,
         page: json["page"],
-        MovieMovieDBs: List<MovieMovieDB>.from(json["MovieMovieDBs"].map((x) => MovieMovieDB.fromJson(x))),
+        results: List<MovieMovieDB>.from(json["results"].map((x) => MovieMovieDB.fromJson(x))),
         totalPages: json["total_pages"],
-        totalMovieMovieDBs: json["total_MovieMovieDBs"],
+        totalResults: json["total_results"],
     );
 
-  get results => null;
-
     Map<String, dynamic> toJson() => {
-        "dates": dates== null ? null : dates!.toJson(),
+        "dates": dates == null ? null : dates!.toJson(),
         "page": page,
-        "MovieMovieDBs": List<dynamic>.from(MovieMovieDBs.map((x) => x.toJson())),
+        "results": List<dynamic>.from(results.map((x) => x.toJson())),
         "total_pages": totalPages,
-        "total_MovieMovieDBs": totalMovieMovieDBs,
+        "total_results": totalResults,
     };
 }
 
 class Dates {
-    final DateTime maximum;
-    final DateTime minimum;
-
     Dates({
         required this.maximum,
         required this.minimum,
     });
+
+    final DateTime maximum;
+    final DateTime minimum;
 
     factory Dates.fromJson(Map<String, dynamic> json) => Dates(
         maximum: DateTime.parse(json["maximum"]),
@@ -53,4 +52,3 @@ class Dates {
         "minimum": "${minimum.year.toString().padLeft(4, '0')}-${minimum.month.toString().padLeft(2, '0')}-${minimum.day.toString().padLeft(2, '0')}",
     };
 }
-
