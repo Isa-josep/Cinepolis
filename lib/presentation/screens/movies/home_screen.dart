cb2bc256b7a71145930aa0c48509c9851fede95b
Isa-josep <isaurini1902@gmail.com>
2024-01-09 23:32:53 -0600

    Home scrren modi

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinepolis/presentation/provider/providers.dart';
+import 'package:cinepolis/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -34,15 +35,22 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     final nowPlayingMovies =ref.watch(nowPlayingMovisProvider);
     //if(nowPlayingMovies.length ==0) return CircularProgressIndicator();
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context,index){
-        final movie =nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-          
-        );
-      }
+    return Column(
+      
+      children: [
+        CustomAppbar(),
+          Expanded(
+            child: ListView.builder(
+            itemCount: nowPlayingMovies.length,
+            itemBuilder: (context,index){
+            final movie =nowPlayingMovies[index];
+            return ListTile(
+            title: Text(movie.title),
+              );
+            }
+          ),
+        )
+      ],
     );
   }
 }
