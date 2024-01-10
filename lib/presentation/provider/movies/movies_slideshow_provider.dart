2ef62ccf95a017b228eaeece970061c77bfcb7a7
Isa-josep <isaurini1902@gmail.com>
2024-01-10 12:14:58 -0600

    hacer funcion para cortar movies

---
+++ lib/presentation/provider/movies/movies_slideshow_provider.dart
@@ -0,0 +1,12 @@
+import 'package:cinepolis/domain/entities/movie.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'movies_providers.dart';
+
+final  moviesSlideshowProvider =Provider<List<Movie>>((ref) {
+
+  final nowPlayingMovies =ref.watch(nowPlayingMovisProvider);
+  
+  if(nowPlayingMovies.isEmpty)return [];
+
+  return nowPlayingMovies.sublist(0,6);
+});
