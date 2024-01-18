76f5026bc70b6b7cda68bcc348abcb00908e9a56
Isa-josep <isaurini1902@gmail.com>
2024-01-17 19:11:06 -0600

    sinopsis movie and  category

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -38,11 +38,79 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
         physics: const ClampingScrollPhysics(), //BouncingScrollPhysics
         slivers: [
           _CustomSliverAppBar(movie: movie),
+          SliverList(delegate: SliverChildBuilderDelegate(
+            (context,index)=>_MovieDetails(movie: movie),
+            childCount: 1,
+          ))
         ],
       ),
     );
   }
 }
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetails({ required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyle =Theme.of(context).textTheme;
+    return  Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width*0.3,
+                ),
+              ),
+              const SizedBox(width: 10,),
+              SizedBox(
+                width: (size.width-40) *0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(movie.title,style: textStyle.titleLarge),
+                    Text(movie.overview)
+                  ],
+                ),
+              )
+            ],
+            
+          ),
+        ),
+
+         Padding(padding: const EdgeInsets.all(8),
+            child: Wrap(
+              children: [
+                ...movie.genreIds.map((gender)=> Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  child: Chip(
+                    label: Text(gender),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(22),
+                        bottomRight: Radius.circular(22),
+                      ),
+                      side: BorderSide(color: Colors.grey)
+                    ),
+                  ),
+                ))
+              ],
+          ),
+        ),
+       const SizedBox(height: 25,),
+      ],
+    );
+  }
+}
+
 
 class _CustomSliverAppBar extends StatelessWidget {
   final Movie movie;
@@ -167,3 +235,20 @@ SizedBox.expand(
 
  */
 
+
+/*
+(context,index){
+              return Container(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  movie.overview,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              );
+            
+            }
+ */
