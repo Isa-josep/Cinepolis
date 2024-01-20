13c1c667c06738e4dbd0918ecaeda9285f80ae2f
Isa-josep <isaurini1902@gmail.com>
2024-01-19 21:30:28 -0600

    barril archivo

--- lib/presentation/provider/providers.dart
+++ lib/presentation/provider/providers.dart
@@ -2,3 +2,5 @@ export 'movies/initial_loading_provider.dart';
 export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
+export 'actors/actor_by_movie_provider.dart';
+export 'actors/actor_repositoru_provider.dart';
