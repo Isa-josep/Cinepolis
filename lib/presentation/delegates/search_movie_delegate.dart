8177d04e07ab034e42358b93b49a8a767019f351
Isa-josep <isaurini1902@gmail.com>
2024-01-22 19:35:44 -0600

    mostrar peliculas y descripcion

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -60,23 +60,74 @@ class SearchMovieDelegate extends SearchDelegate<Movie?>{
         return ListView.builder(
             itemCount: movies.length,
             itemBuilder: (context,index){
-              final movie = movies[index];
+              return _MovieItem(movie: movies[index]);
+            }
+        );
+      }
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  const _MovieItem({
+    required this.movie
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
+    final size= MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5), 
+      child: Row(
+        children: [
+          SizedBox(
+            width: size.width*0.2,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                movie.posterPath
+              ),
+            ),
+          ),
+          const SizedBox(width: 10,),
+          SizedBox(
+            width: size.width*0.7,
+            child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                Text(movie.title,style: textStyle.titleMedium),
+                (movie.overview.length>100)
+                ? Text('${movie.overview.substring(0,100)}...')
+                : Text(movie.overview)
+               ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/* 
+
+final movie = movies[index];
               return ListTile(
 
-                leading: FadeInImage(
-                  placeholder: const AssetImage('assets/images/no-image.jpg'), 
-                  image: NetworkImage(movie.fullPosterImg),
-                  width: 50,
-                  fit: BoxFit.contain,
-                ),
+                // leading: FadeInImage(
+                //   placeholder: const AssetImage('assets/loading.gif'), 
+                //   image: NetworkImage(movie.posterPath),
+                //   width: 50,
+                //   fit: BoxFit.contain,
+                // ),
+                
                 title: Text(movie.title),
                 subtitle: Text(movie.originalTitle),
                 onTap: (){
                   context.go('/movie/${movie.id}');
                 },);
           },
-        );
-      }
-    );
-  }
-}
+
+
+*/
