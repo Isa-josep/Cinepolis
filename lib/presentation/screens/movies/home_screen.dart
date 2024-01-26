4a6ab4a482f12236b6979eb8f8654d2ca18d3e17
Isa-josep <isaurini1902@gmail.com>
2024-01-26 14:21:56 -0600

    configuracion del go router

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -3,13 +3,25 @@ import 'package:cinepolis/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   static const name = 'home_screen';
-  const HomeScreen({super.key});
+  final int pageIndex;
+  const HomeScreen({
+    super.key, 
+    required this.pageIndex
+  });
 
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(),
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: FavoritesView(),
+    return  Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
      bottomNavigationBar:CustomButtomNavigation() ,
     );
   }
