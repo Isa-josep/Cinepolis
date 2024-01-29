3c791c39484ee2cb1d33cce1038425e2fdacfc16
Isa-josep <isaurini1902@gmail.com>
2024-01-28 19:37:53 -0600

    favorites movies view

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,17 +1,40 @@
+import 'package:cinepolis/presentation/provider/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
-  const FavoritesView({super.key});
+class FavoritesView extends ConsumerStatefulWidget {
+   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+
   Widget build(BuildContext context) {
+    final favoriteMovies=ref.watch(favoriteMoviesProvider).values.toList(); 
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Favorites'),
       ),
-      body: const Center(
-        child: Text('Favorites'),
-      ),
+      body: ListView.builder(
+        itemCount: favoriteMovies.length,
+        itemBuilder: (context,index){
+          final movies = favoriteMovies[index];
+          return ListTile(
+            title: Text(movies.title),
+          );
+        }
+      )
     );
   }
 }
