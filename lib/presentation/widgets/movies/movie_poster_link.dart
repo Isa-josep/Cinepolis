a00a1e66d092adab99aa55ffd1d0bceefe79e2cf
Isa-josep <isaurini1902@gmail.com>
2024-01-30 22:32:55 -0600

    animacion infinite scroll

--- lib/presentation/widgets/movies/movie_poster_link.dart
+++ lib/presentation/widgets/movies/movie_poster_link.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinepolis/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -10,18 +11,20 @@ class MoviePosterLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: ()=> context.push('/home/0/movie/${movie.id} ') ,
-      child: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(5),
-          bottomLeft: Radius.circular(5),
-          bottomRight: Radius.circular(20),
-        ),
-        child: Image.network(
-          movie.posterPath,
-          fit: BoxFit.cover,
+    return FadeInDown(
+      child: GestureDetector(
+        onTap: ()=> context.push('/home/0/movie/${movie.id} ') ,
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(5),
+            bottomLeft: Radius.circular(5),
+            bottomRight: Radius.circular(20),
+          ),
+          child: Image.network(
+            movie.posterPath,
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
