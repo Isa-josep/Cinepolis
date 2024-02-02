1257fd7280d31dc517808ddf2052b29bc216ff67
Isa-josep <isaurini1902@gmail.com>
2024-02-01 23:10:19 -0600

    Add conditional rendering for empty favorite movies list

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinepolis/presentation/provider/providers.dart';
 import 'package:cinepolis/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
    const FavoritesView({super.key});
@@ -37,6 +38,42 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
 
   Widget build(BuildContext context) {
     final favoriteMovies=ref.watch(favoriteMoviesProvider).values.toList(); 
+
+    if(favoriteMovies.isEmpty){
+      final color = Theme.of(context).colorScheme;
+      return  Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              Icon(Icons.favorite_border_outlined, size: 100, color: color.primary),
+              const SizedBox(height: 20),
+              Text(
+                'ohhh no!!',
+                style: TextStyle(
+                  fontSize: 50,
+                  color: color.primary,
+                ),
+              ),
+              const Text(
+                'No tienes peliculas favoritas',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black45,
+                ),
+              ),
+              const SizedBox(height: 20),
+              FilledButton.tonal(
+                onPressed: (){
+                  context.go('/home/0');
+                }, 
+                child: const Text('Explorar')
+              )
+            ],
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
